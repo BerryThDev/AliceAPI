f024a673292d9a9a597f73ac7c9bcf4575b12e83
Nik <61439310+NikV2@users.noreply.github.com>
2020-11-20 02:00:25 +0200

    Commit

--- src/main/java/me/nik/alice/api/events/PlayerViolationEvent.java
+++ src/main/java/me/nik/alice/api/events/PlayerViolationEvent.java
@@ -1,21 +1,31 @@
 package me.nik.alice.api.events;
 
 import org.bukkit.entity.Player;
+import org.bukkit.event.Cancellable;
 import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
 
-public class PlayerViolationEvent extends Event {
+public class PlayerViolationEvent extends Event implements Cancellable {
 
     private static final HandlerList handlers = new HandlerList();
 
     private final Player player;
     private final String check;
     private final int vl;
+    private boolean cancel = false;
 
     public PlayerViolationEvent(Player player, String check, int vl) {
         this.player = player;
         this.check = check;
         this.vl = vl;
+    }
+
+    public boolean isCancelled() {
+        return this.cancel;
+    }
+
+    public void setCancelled(boolean cancel) {
+        this.cancel = cancel;
     }
 
     /**
